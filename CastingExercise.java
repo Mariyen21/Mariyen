39672a3dcf68bed5f589b7839aec7e3fec435244
Mariyen21 <114734590+Mariyen21@users.noreply.github.com>
2022-10-06 15:25:21 +0530

    Create CastingExercise.java

---
+++ CastingExercise.java
@@ -0,0 +1,13 @@
+public class CastingExercise { 
+public static void main(String[] args) { 
+byte b=10; 
+ 
+int i=b;
+byte c=i;
+byte d=(byte)i;// Type casting makes it possible to store compatible types.byte x=10; 
+byte y=20; 
+Pbyte sum=x*y; // Error is thrown because when two bytes are used in a 
+arthmetic operation, the result will be integer
+ 
+ } 
+} 
